11d0f5b7bece2606c14756fc139c132b36879394
CodeYouness <ylijassi@gmail.com>
2024-10-18 17:50:54 +0200

    added function to check if length of input is >9

--- script.js
+++ script.js
@@ -1,6 +1,25 @@
 const input = document.getElementById('password');
+const inputLength = document.getElementById('length')
+const inputNumber = document.getElementById('number')
+const inputUppercase = document.getElementById('uppercase')
+const inputCharacter = document.getElementById('special-character')
 
-addEventListener('input', function () {
-    const password = input.value
-    console.log(password)
-})
+// event listener per sentire se avviene un cambiamento
+// "input" -> tipo di event listener
+function passwordCheck() {
+
+    const password = input.value;
+    let strength = 0;
+
+    if (password.length >= 9) {
+        inputLength.textContent = "✅";
+        inputLength.classList.add("green");
+        inputLength.classList.remove("red");
+        strength++;
+    } else {
+        inputLength.textContent = "❌";
+        inputLength.classList.add("red");
+        inputLength.classList.remove("green");
+    }
+}
+input.addEventListener('input', passwordCheck);
